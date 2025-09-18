278ac1db44b170b81c0bed4788051e0ec4ac4d5a
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-18 20:08:06 +0100

    🧪 Add AuditFieldsSpecs tests for Created and Updated fields on visit status transition

---
+++ tests/SmartGate.Domain.Tests/AuditFieldsSpecs.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+
+namespace SmartGate.Domain.Tests;
+
+public class AuditFieldsSpecs
+{
+    [Fact]
+    public void Created_and_Updated_set_on_create_and_updated_on_transition()
+    {
+        var t0 = new DateTime(2025, 9, 17, 8, 30, 0, DateTimeKind.Utc);
+        var visit = TestData.VisitWith(nowUTC: t0);
+        visit.CreatedAtUTC.Should().Be(t0);
+        visit.UpdatedAtUTC.Should().Be(t0);
+
+        var t1 = t0.AddMinutes(5);
+        visit.UpdateStatus(Visits.VisitStatus.AtGate, t1);
+        visit.UpdatedAtUTC.Should().Be(t1);
+        visit.CreatedAtUTC.Should().Be(t0);
+    }
+}
