08bd890fb53cd6487cd7ce97d8f3b69bbeae2cbf
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-18 17:29:20 +0100

    ✨ Add Activity entity with validation for unit number

---
+++ src/SmartGate.Domain/Visits/Entities/Activity.cs
@@ -0,0 +1,34 @@
+using SmartGate.Domain.Common;
+
+namespace SmartGate.Domain.Visits.Entities;
+
+public sealed class Activity
+{
+    public const int MaxUnitLength = 10;
+
+    public Guid Id { get; }
+    public ActivityType Type { get; }
+    public string UnitNumberRaw { get; }
+    public string UnitNumberNormalized { get; }
+
+    public Activity(ActivityType type, string unitNumberRaw, Guid? id = null)
+    {
+        if (string.IsNullOrEmpty(unitNumberRaw))
+            throw new NullReferenceInAggregateException(nameof(unitNumberRaw));
+
+        if (unitNumberRaw.Length > MaxUnitLength)
+            throw new MaxLengthExceededException(nameof(unitNumberRaw), MaxUnitLength);
+
+        this.Id = id ?? Guid.NewGuid();
+        this.Type = type;
+
+        this.UnitNumberRaw = unitNumberRaw;
+        var normalized = Normalization.NormalizePlateOrUnit(unitNumberRaw);
+        
+        if (string.IsNullOrWhiteSpace(normalized))
+            throw new InvalidIdentifierException(nameof(this.UnitNumberRaw));
+
+        this.UnitNumberNormalized = normalized;
+    }
+
+}
