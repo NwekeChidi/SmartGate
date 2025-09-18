00d71a3c9305677f788cd26cff1eb003559e9c82
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-18 19:56:38 +0100

    🧪 Add VisitStatusTerminalStateSpecs tests for Completed status as terminal

---
+++ tests/SmartGate.Domain.Tests/VisitStatusTerminalStateSpecs.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using SmartGate.Domain.Visits;
+
+namespace SmartGate.Domain.Tests;
+
+public class VisitStatusTerminalStateSpecs
+{
+    [Fact]
+    public void Completed_is_terminal()
+    {
+        var visit = TestData.VisitWith();
+        visit.UpdateStatus(VisitStatus.AtGate);
+        visit.UpdateStatus(VisitStatus.OnSite);
+        visit.UpdateStatus(VisitStatus.Completed);
+
+        var act = () => visit.UpdateStatus(VisitStatus.Completed);
+        act.Should().Throw<CompletedIsTerminalException>();
+    }
+}
