c6699c5831b3667935a759209e71c95cd5dff3b8
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-20 23:24:34 +0100

    🎨 Clean up validator

--- src/SmartGate.Application/Visits/Validators/CreateVisitRequestValidator.cs
+++ src/SmartGate.Application/Visits/Validators/CreateVisitRequestValidator.cs
@@ -10,18 +10,26 @@ public sealed partial class CreateVisitRequestValidator : AbstractValidator<Crea
 
     public CreateVisitRequestValidator()
     {
-        RuleFor(x => x.TruckLicensePlate).NotEmpty().MaximumLength(32);
+        RuleFor(x => x.TruckLicensePlate)
+            .NotEmpty()
+            .MinimumLength(6)
+            .MaximumLength(32);
 
         RuleFor(x => x.Driver).NotNull();
-        RuleFor(x => x.Driver.FirstName).NotEmpty().MaximumLength(128);
-        RuleFor(x => x.Driver.LastName).NotEmpty().MaximumLength(128);
+        RuleFor(x => x.Driver.FirstName)
+            .NotEmpty()
+            .MinimumLength(1)
+            .MaximumLength(128);
+        RuleFor(x => x.Driver.LastName)
+            .NotEmpty()
+            .MinimumLength(1)
+            .MaximumLength(128);
         RuleFor(x => x.Driver.Id)
             .NotEmpty()
             .Must(DriverIdRegex.IsMatch)
             .WithMessage("Driver.Id must match pattern DFDS-<1..6 alphanumeric characters>.");
 
-        RuleFor(x => x.Activities).NotNull().Must(a => a.Count > 0).WithMessage("At least one activity is required");
-
+        RuleFor(x => x.Activities).NotNull().Must(activity => activity.Count > 0).WithMessage("At least one activity is required");
         RuleForEach(x => x.Activities).ChildRules(activity =>
         {
             activity.RuleFor(a => a.UnitNumber)
@@ -29,13 +37,13 @@ public sealed partial class CreateVisitRequestValidator : AbstractValidator<Crea
         });
 
         RuleFor(x => x.IdempotencyKey)
-            .MaximumLength(128)
-            .When(x => x.IdempotencyKey is not null);
-        
+            .NotEqual(Guid.Empty)
+            .When(x => x.IdempotencyKey is not null)
+            .WithMessage("IdempotencyKey must be a valid .");
+
         RuleFor(x => x.Status)
             .Equal(VisitStatus.PreRegistered)
-            .WithMessage("New visits must have status 'PreRegistered'")
-            .When(x => x.Status is not null);
+            .WithMessage("New visits must have status 'PreRegistered'");
     }
 
     [GeneratedRegex(@"^(?i)dfds-[a-z0-9]{1,6}$", RegexOptions.Compiled, "en-US")]
