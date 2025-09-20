e596ee83eb5abe0d02c8f35df969bd052b91f031
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-20 23:27:18 +0100

    🎨 Move OutboxMessage class definition into OutboxMessageConfiguration for better organization

--- src/SmartGate.Infrastructure/Database/Setup/OutboxMessageConfiguration.cs
+++ src/SmartGate.Infrastructure/Database/Setup/OutboxMessageConfiguration.cs
@@ -3,6 +3,15 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace SmartGate.Infrastructure.Database.Setup;
 
+public class OutboxMessage
+{
+    public Guid Id { get; set; }
+    public DateTime OccurredAtUTC { get; set; }
+    public string Type { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public DateTime? ProcessedAtUTC { get; set; }
+}
+
 public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
 {
     public void Configure(EntityTypeBuilder<OutboxMessage> eb)
