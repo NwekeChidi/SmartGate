241e4e2f2266f2c9310bcd6caf8b47bbddcf53bd
Chidimma Dominic <realchidinweke@gmail.com>
2025-09-22 12:35:18 +0100

    ♻️ Refactor CreateVisitRequestValidator to improve validation rules and error messages

--- src/SmartGate.Application/Visits/Validators/CreateVisitRequestValidator.cs
+++ src/SmartGate.Application/Visits/Validators/CreateVisitRequestValidator.cs
@@ -8,14 +8,14 @@ namespace SmartGate.Application.Visits.Validators;
 public sealed partial class CreateVisitRequestValidator : AbstractValidator<CreateVisitRequest>
 {
     private static readonly Regex DriverIdRegex = _driverIdRegex();
+    private static readonly Regex UnitNumberRegex = _unitNumberRegex();
+    private const string cultureName = "en-GB";
 
     public CreateVisitRequestValidator()
     {
         RuleFor(x => x.TruckLicensePlate)
             .NotEmpty()
-            .MinimumLength(7)
-            .MaximumLength(32)
-            .WithMessage("TruckLicensePlate must be valid and 7 characters long.");
+            .Length(7);
 
         RuleFor(x => x.Driver).NotNull();
         RuleFor(x => x.Driver.FirstName)
@@ -26,28 +26,32 @@ public sealed partial class CreateVisitRequestValidator : AbstractValidator<Crea
             .MaximumLength(128);
         RuleFor(x => x.Driver.Id)
             .NotEmpty()
-            .MinimumLength(16)
             .Must(DriverIdRegex.IsMatch)
-            .WithMessage("Driver.Id must match pattern DFDS-<11 numeric characters>.");
+            .WithMessage("driver.id must match pattern DFDS-<11 numeric characters>.")
+            .Length(16);
 
         RuleFor(x => x.Activities).NotNull().Must(activity => activity.Count > 0).WithMessage("At least one activity is required");
         RuleForEach(x => x.Activities).ChildRules(activity =>
         {
             activity.RuleFor(a => a.UnitNumber)
-                .NotEmpty().MinimumLength(10).MaximumLength(32)
-                .WithMessage("UnitNumber must be exactly 10 characters.");
+                .NotEmpty()
+                .Must(UnitNumberRegex.IsMatch)
+                .WithMessage("activity.unitNumber must match pattern DFDS<6 numeric characters>.")
+                .Length(10);
         });
 
         RuleFor(x => x.IdempotencyKey)
             .NotEqual(Guid.Empty)
             .When(x => x.IdempotencyKey is not null)
-            .WithMessage("IdempotencyKey must be a valid Guid.");
+            .WithMessage("idempotencyKey must be a valid Guid.");
 
         RuleFor(x => x.Status)
             .Equal(VisitStatus.PreRegistered)
             .WithMessage("New visits must have status 'PreRegistered'");
     }
 
-    [GeneratedRegex(@"^(?i)dfds-[0-9]{1,11}$", RegexOptions.Compiled, "en-GB")]
+    [GeneratedRegex(@"^(?i)dfds-[0-9]{11}$", RegexOptions.Compiled, cultureName)]
     private static partial Regex _driverIdRegex();
+    [GeneratedRegex(@"^(?i)dfds[0-9]{6}$", RegexOptions.Compiled, cultureName)]
+    private static partial Regex _unitNumberRegex();
 }
